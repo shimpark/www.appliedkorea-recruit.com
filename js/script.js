fc7904fab82c8018eacf702a1b505c9864e7d0a8
심재운 <shimpark@gmail.com>
2021-10-15 12:12:53 +0900

    초기화

---
+++ js/script.js
@@ -0,0 +1,121 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+var agent = navigator.userAgent.toLowerCase();
+var trident = navigator.userAgent.match(/Trident\/(\d.\d)/i);
+
+var _w;
+var _wrap;
+var _wid;
+
+var _breakpoint = 720;
+var _breakpointDesktop = 1100;
+
+
+$(function(){
+    init();
+});
+
+function init(){
+    create();
+    addEvent();
+}
+
+function create(){
+    _w = $(window);
+    _wrap = $('#wrap');
+    _wid = _w.width();
+}
+
+function addEvent(){
+    pageMove('.page-move');
+
+    _w.on("resize", resizeEvent);
+    resizeEvent();
+
+    _w.on("scroll", scrollEvent);
+    scrollEvent();
+}
+
+
+function scrollEvent(){
+    var st = $(window).scrollTop();
+    var sh = $(window).height() / 1.2;
+    var section = $('.section');
+
+    section.each(function(i){
+        if( st > section.eq(i).offset().top - sh){
+            $(this).addClass('active');
+        }
+         else {
+            //$(this).removeClass('active');
+        }
+    });
+
+}
+
+
+
+function resizeEvent(){
+    _wid = _w.width();
+
+    $('.responsive').each(function() {
+        var $src = $(this).attr("src");
+        var val = (_wid > _breakpoint) ? $src.replace('mobile', 'pc') : $src.replace('pc', 'mobile');
+
+        $(this).attr("src", val);
+    });
+
+}
+
+function pageMove($selector, $position){
+	if($position == undefined) $position = 0;
+
+	var selector = $selector;
+	$(selector).on('click', function (e) {
+		e.preventDefault();
+
+		var _top = $($(this).attr('href'));
+		var $target = _top.offset().top;
+   
+
+		$('html,body').animate({
+			scrollTop: $target+$position
+		}, 500);
+
+
+	});
+}
+
+
+function popupClose($dimName, $idName){
+    var dimName = $dimName;
+    var $popupLayer = $("#"+$idName);
+    $(dimName).hide();
+    $popupLayer.hide();
+}
+
+function popupOpen($dimName, $idName){
+    var dimName = $dimName;
+    var $popupLayer = $("#"+$idName);
+    $(dimName).show();
+    $popupLayer.show();
+    popupPosition($popupLayer);
+}
+
+
+function popupPosition($popupLayer) {
+    var st = $(window).scrollTop();
+    var winHeight = $(window).height();
+    var popupHeight = $popupLayer.outerHeight();
+
+    var topValue = (st + ( winHeight / 2 - popupHeight / 2 ));
+    if($(window).height() < popupHeight){
+        topValue = st;
+    }
+
+    $popupLayer.css({top:topValue});
+}
+
+
+
+
+
